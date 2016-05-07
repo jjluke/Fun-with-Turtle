4a46d2c15ee7960de16477b6b0c527340ebe2026
jjluke <jj.luke808@gmail.com>
2016-05-06 17:35:34 -0700

    Update star.py

--- star.py
+++ star.py
@@ -1,23 +1,24 @@
 import turtle
 import random
 wn = turtle.Screen()
-name = "Ian"
-wn.bgcolor("lightblue")
+wn.bgcolor("black")
 snappy = turtle.Turtle()
-snappy.speed(20)
-snappy.color("red")
-snappy.pensize(5)
-#star pattern
+snappy.speed(100)
+snappy.color("white")
+snappy.pensize(10)
 degree = 144
 length = 200
-snappy.speed(1)
-snappy.forward(length)
-snappy.right(degree)
-snappy.forward(length)
-snappy.right(degree)
-snappy.forward(length)
-snappy.right(degree)
-snappy.forward(length)
-snappy.right(degree)
-snappy.forward(length)
+while True:
+  snappy.speed(10)
+  snappy.forward(length)
+  snappy.right(degree)
+  snappy.forward(length)
+  snappy.right(degree)
+  snappy.forward(length)
+  snappy.right(degree)
+  snappy.forward(length)
+  snappy.right(degree)
+  snappy.forward(length)
+  length =- 100
+  degree =- 100
 wn.exitonclick()
