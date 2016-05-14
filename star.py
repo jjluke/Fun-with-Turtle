7bc4819fbde25b4b9613ee0e5dea0c8b49a0edff
jjluke <jj.luke808@gmail.com>
2016-05-14 14:32:41 -0700

    Update star.py

--- star.py
+++ star.py
@@ -5,20 +5,23 @@ wn.bgcolor("black")
 snappy = turtle.Turtle()
 snappy.speed(100)
 snappy.color("white")
-snappy.pensize(10)
+snappy.pensize(5)
 degree = 144
 length = 200
+snappy.speed(5)
 while True:
-  snappy.speed(10)
   snappy.forward(length)
+  snappy.color("red")
   snappy.right(degree)
   snappy.forward(length)
+  snappy.color("yellow")
   snappy.right(degree)
   snappy.forward(length)
+  snappy.color("green")
   snappy.right(degree)
   snappy.forward(length)
+  snappy.color("pink")
   snappy.right(degree)
   snappy.forward(length)
-  length =- 100
-  degree =- 100
-wn.exitonclick()
+  snappy.color("white")
+  wn.exitonclick()
